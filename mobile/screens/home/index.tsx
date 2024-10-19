dc5f5c64fcd7edacbf582084912a3ddf16d2e180
Josh Loesch <jloesch30@gmail.com>
2024-10-19 08:26:15 -0500

    Landing screen event chooser

--- mobile/screens/home/index.tsx
+++ mobile/screens/home/index.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, Text, View } from 'react-native';
 import { styles } from './styles';
 import Button from "common/button";
+import TouchableImage from 'common/touchable-image';
 
 interface HomeProps {
   navigation: any;
@@ -11,15 +12,47 @@ const Home = ({ navigation }: HomeProps) => {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <Text >Welcome to React Native Web!</Text>
-        <StatusBar style="auto" />
-        <Button
-          onPress={() => navigation.navigate('Main')}
+        <Text
+          style={{
+            fontSize: 24,
+            fontWeight: 'bold',
+            marginBottom: 10,
+          }}
         >
-          <Text>
-            Hello there
-          </Text>
-        </Button>
+          Welcome to the WWBF
+        </Text>
+        <Text
+          style={{
+            fontSize: 18,
+          }}
+        >
+          Please select a location
+        </Text>
+        <View style={{
+          flexDirection: 'column',
+          gap: 50,
+          marginTop: 50,
+        }}
+        >
+          <TouchableImage
+            onPress={() => navigation.navigate('Main')}
+            image={require('assets/WWBF-KATYImage.png')}
+            style={{
+              width: 200,
+              height: 200,
+              marginBottom: 10,
+            }}
+          />
+          <TouchableImage
+            onPress={() => navigation.navigate('Main')}
+            image={require('assets/WWBF-PflugervilleImage.png')}
+            style={{
+              width: 200,
+              height: 200,
+              marginBottom: 10,
+            }}
+          />
+        </View>
       </View>
     </SafeAreaView>
   );
