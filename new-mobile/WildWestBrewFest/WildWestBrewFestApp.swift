8b5458e5f6f0db3e91127a8e3c0bf5b5549a9cc5
Josh Loesch <jloesch30@gmail.com>
2025-01-30 21:17:33 -0600

    Swift FE replacement

---
+++ new-mobile/WildWestBrewFest/WildWestBrewFestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WildWestBrewFestApp.swift
+//  WildWestBrewFest
+//
+//  Created by Josh Loesch on 1/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WildWestBrewFestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
